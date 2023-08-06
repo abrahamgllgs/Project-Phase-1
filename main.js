e64123802886450f89bcb6c1e55074579f279e88
Abraham Gallegos <abrahamgllgs@gmail.com>
2023-08-06 12:35:00 -0500

    Added const in my code as well as fetch data

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 function fetchHoroscope(sunSign) {
   const fetchHoroscopeApiURL =
     "https://aztro.sameerkumar.website/?sign=${sunSign}&day=today;";
+
+  return fetch(ApiURL, { method: "POST" })
+    .then((respond) => respond.json())
+    .then((data) => data.description);
+}
+
+function getHoroscope(birthday) {
+  const month = birthday.getmonth() + 1;
+  const day = birthday.getDate();
+
+  const horosocopes = {
+    2: "Aquarius",
+    3: "Pisces",
+    4: "Aries",
+    5: "Taurus",
+    6: "Gemini",
+    7: "Cancer",
+    8: "Leo",
+    9: "Virgo",
+    10: "Libra",
+    11: "Scorpio",
+    12: "Sagittarius",
+  };
 }
 
 document

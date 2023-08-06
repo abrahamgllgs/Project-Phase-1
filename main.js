d2f60cac07d2973493e5e2aba89169de76232368
Abraham Gallegos <abrahamgllgs@gmail.com>
2023-08-06 11:38:52 -0500

    Added some changes to style.css to fit project

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-console.log('main.js connected')
+console.log("main.js connected");
 
-const handleFormInputFocus = () => {
-    console.log('focus occurred')
+function getHoroscope(birthdate) {
+  const month = birthdate.getMonth() + 1;
+  const day = birthdate.getDate();
 }
+const getHoroscopeSign = async () => {
+  const horoscopeSignApiURL =
+    "https://aztro.sameerkumar.website?sign=aries&day=today"; // Replace with the desired sign and day
 
-const searchTermsInput = document.body.querySelector("#search-terms")
-
-searchTermsInput.addEventListener('focus', handleFormInputFocus)
+  try {
+    const response = await fetch(horoscopeSignApiURL);
+    const data = await response.json(); // Await the response.json()
+    console.log("data: ", data);
+  } catch (error) {
+    console.log(error);
+    alert("something went wrong");
+  }
+};
+await getHoroscopeSign();
+document.getElementById("findHoroscope").addEventListener("click", function () {
+  const birthdayInput = document.getElementById("birthday");
+  const birthday = new Date(birthdayInput.value);
+});

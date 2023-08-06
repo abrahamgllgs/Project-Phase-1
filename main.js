13619fa1772171eacc79edc7f4f1b6e3490682dc
Abraham Gallegos <abrahamgllgs@gmail.com>
2023-08-06 11:58:29 -0500

    Added an event listener to collect data using Api

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-console.log("main.js connected");
-
-function getHoroscope(birthdate) {
-  const month = birthdate.getMonth() + 1;
-  const day = birthdate.getDate();
+function fetchHoroscope(sunSign) {
+  const fetchHoroscopeApiURL =
+    "https://aztro.sameerkumar.website/?sign=${sunSign}&day=today;";
 }
-const getHoroscopeSign = async () => {
-  const horoscopeSignApiURL =
-    "https://aztro.sameerkumar.website?sign=aries&day=today"; // Replace with the desired sign and day
 
-  try {
-    const response = await fetch(horoscopeSignApiURL);
-    const data = await response.json(); // Await the response.json()
-    console.log("data: ", data);
-  } catch (error) {
-    console.log(error);
-    alert("something went wrong");
-  }
-};
-await getHoroscopeSign();
-document.getElementById("findHoroscope").addEventListener("click", function () {
-  const birthdayInput = document.getElementById("birthday");
-  const birthday = new Date(birthdayInput.value);
-});
+document
+  .getElementById("findHoroscope")
+  .addEventListener("click", async function () {
+    const birthdayInput = document.getElementById("birthday");
+    const birthday = new Date(birthdayInput.value);
+    const sunSign = getHoroscope(birthday);
+  });

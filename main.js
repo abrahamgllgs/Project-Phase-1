3d7dbc56955ae5f901d8c3a409804e74ad2116a9
Abraham Gallegos <abrahamgllgs@gmail.com>
2023-08-06 15:12:39 -0500

    style.css changes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function getHoroscope(birthdate) {
   const month = birthdate.getMonth() + 1;
   const day = birthdate.getDate();
-  const horoscopes = {
+  const horoscope = {
     2: "Aquarius",
     3: "Pisces",
     4: "Aries",
@@ -16,37 +16,36 @@ function getHoroscope(birthdate) {
   };
 
   if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
-    return horoscopes[4];
+    return horoscope[4];
   } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
-    return horoscopes[5];
+    return horoscope[5];
   } else if ((month === 5 && day >= 21) || (month === 6 && day <= 20)) {
-    return horoscopes[6];
+    return horoscope[6];
   } else if ((month === 6 && day >= 21) || (month === 7 && day <= 22)) {
-    return horoscopes[7];
+    return horoscope[7];
   } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
-    return horoscopes[8];
+    return horoscope[8];
   } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
-    return horoscopes[9];
+    return horoscope[9];
   } else if ((month === 9 && day >= 23) || (month === 10 && day <= 22)) {
-    return horoscopes[10];
+    return horoscope[10];
   } else if ((month === 10 && day >= 23) || (month === 11 && day <= 21)) {
-    return horoscopes[11];
+    return horoscope[11];
   } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
-    return horoscopes[12];
+    return horoscope[12];
   } else if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
-    return horoscopes[1];
+    return horoscope[1];
   } else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
-    return horoscopes[2];
+    return horoscope[2];
   } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
-    return horoscopes[3];
+    return horoscope[3];
   } else {
     return "Invalid date";
   }
 }
 
 function fetchHoroscope(sign, day) {
-  const apiUrl = `https://aztro.sameerkumar.website?sign= <sign> &day= <day>`;
-
+  const apiUrl = `https://aztro.sameerkumar.website?sign=${sign}&day=${day}`;
   return fetch(apiUrl, { method: "POST" })
     .then((response) => response.json())
     .then((data) => data.description);
@@ -62,9 +61,17 @@ document
     try {
       const horoscopeResult = document.getElementById("horoscopeResult");
       const horoscope = await fetchHoroscope(sunSign, "today");
-
-      horoscopeResult.textContent = "Your horoscope for today is ${horoscope }";
+      horoscopeResult.textContent = `Your horoscope for today is ${horoscope}`;
     } catch (error) {
       console.error("Error fetching horoscope", error);
     }
   });
+
+document.getElementById("birthday").addEventListener("change", function () {
+  const birthdayInput = document.getElementById("birthday");
+  const birthday = new Date(birthdayInput.value);
+  const sunSign = getHoroscope(birthday);
+
+  const horoscopeResult = document.getElementById("horoscopeResult");
+  horoscopeResult.textContent = `Your sun sign is: ${sunSign}.`;
+});
